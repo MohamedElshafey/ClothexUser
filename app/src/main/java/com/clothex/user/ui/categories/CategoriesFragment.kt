cd3babecf5dab2b1021ded0a75d1c1e887440eb5
Mohamed Elshafey <mohamed.elshafey@halan.com>
2021-11-18 00:10:40 +0200

    init commit

---
+++ app/src/main/java/com/clothex/user/ui/categories/CategoriesFragment.kt
@@ -0,0 +1,40 @@
+package com.clothex.user.ui.categories
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.clothex.user.databinding.FragmentCategoriesBinding
+
+class CategoriesFragment : Fragment() {
+
+    private lateinit var categoriesViewModel: CategoriesViewModel
+    private var _binding: FragmentCategoriesBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        categoriesViewModel = ViewModelProvider(this)[CategoriesViewModel::class.java]
+
+        _binding = FragmentCategoriesBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textNotifications
+        categoriesViewModel.text.observe(viewLifecycleOwner, Observer {
+            textView.text = it
+        })
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/com/clothex/user/ui/wishlist/WishListViewModel.kt
@@ -0,0 +1,13 @@
+package com.clothex.user.ui.wishlist
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class WishListViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is dashboard Fragment"
+    }
+    val text: LiveData<String> = _text
+}
